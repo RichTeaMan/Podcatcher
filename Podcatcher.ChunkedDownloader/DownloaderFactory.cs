0172e1bf46ca3f855ff8c0b654c715ab74665bdc
Thomas Holmes <ThomasHolmes@outlook.com>
2015-10-07 14:57:42 +0100

    Added downloader factory.

---
+++ Podcatcher.ChunkedDownloader/DownloaderFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Podcatcher.ChunkedDownloader
+{
+	public static class DownloaderFactory
+	{
+
+		public static Downloader GetDownloader(string url, string filename) {
+			var httpStore = new HttpDataStore(url);
+			var fileStore = new FileDataStore(filename);
+
+			var downloader = new Downloader (fileStore, httpStore);
+			return downloader;
+		}
+	}
+}
+
