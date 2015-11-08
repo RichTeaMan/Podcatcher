71ae6cec9e28bed92b991537a28bba1e97900cb2
Thomas Holmes <thomasholmes@outlook.com>
2015-11-08 19:53:26 +0000

    Added more download tests.

--- Podcatcher.Downloader.Tests/ChunkedDownloaderTest.cs
+++ Podcatcher.Downloader.Tests/ChunkedDownloaderTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Linq;
 using Podcatcher.Domain;
 
 namespace Podcatcher.Downloader.Tests
@@ -10,6 +11,7 @@ namespace Podcatcher.Downloader.Tests
     public class ChunkedDownloaderTest
     {
         const string RESOURCE_LINK = "https://upload.wikimedia.org/wikipedia/commons/9/96/Macrocranion_tupaiodon_01.jpg";
+        const int RESOURCE_LENGTH = 8248016;
 
         ChunkedDownloader Downloader;
 
@@ -28,7 +30,11 @@ namespace Podcatcher.Downloader.Tests
             using (var wc = new HttpClient())
             using (var response = await wc.SendAsync(message))
             {
+                
+                //var lengthHeader = response.Headers.GetValues("content-length");
+                //var length = int.Parse(lengthHeader.First());
                 Assert.AreEqual(200, (int)response.StatusCode);
+                //Assert.AreEqual(RESOURCE_LENGTH, length);
             }
         }
 
@@ -41,6 +47,28 @@ namespace Podcatcher.Downloader.Tests
 
             Assert.AreEqual(chunkInfo.Start, chunk.Start);
             Assert.AreEqual(chunkInfo.Length, chunk.Length);
+        }
+
+        [TestMethod]
+        public async Task DownloadNearEnd()
+        {
+            // get 1kb from near end
+            var chunkInfo = new ChunkInfo(RESOURCE_LENGTH - 512, 1024);
+            var chunk = await Downloader.DownloadChunk(RESOURCE_LINK, chunkInfo);
+
+            Assert.AreEqual(chunkInfo.Start, chunk.Start);
+            Assert.AreEqual(512, chunk.Length);
+        }
+
+        //[TestMethod]
+        public async Task DownloadEnd()
+        {
+            // get 1kb from near end
+            var chunkInfo = new ChunkInfo(RESOURCE_LENGTH, 1024);
+
+            var chunk = await Downloader.DownloadChunk(RESOURCE_LINK, chunkInfo);
+            Assert.AreEqual(chunkInfo.Start, chunk.Start);
+            Assert.AreEqual(0, chunk.Length);
         }
     }
 }
