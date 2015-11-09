c215487dfbbbdfde952cdc10387e1573a3b33ccd
Thomas Holmes <thomasholmes@outlook.com>
2015-11-09 22:08:32 +0000

    Added command line parser project.

--- Podcatcher.CLI/Program.cs
+++ Podcatcher.CLI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLineParser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,39 @@ namespace Podcatcher.CLI
     {
         static void Main(string[] args)
         {
+            MethodInvoker command = null;
+            try
+            {
+                command = ClCommandAttribute.GetCommand(typeof(Program), args);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error parsing command:");
+                Console.WriteLine(ex.Message);
+            }
+            if (command != null)
+            {
+                try
+                {
+                    command.Invoke();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error running command:");
+                    Console.WriteLine(ex.Message);
+
+                    var inner = ex.InnerException;
+                    while (inner != null)
+                    {
+                        Console.WriteLine(inner);
+                        Console.WriteLine();
+                        inner = inner.InnerException;
+                    }
+
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
         }
     }
 }
